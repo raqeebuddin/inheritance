bc20178b75aa4e1bad8cada04480c0e366d8481a
raqeebuddin <raqeebuddin@hotmail.co.uk>
2017-09-26 15:44:35 +0100

    Car class inheritance from Vehicle class added

---
+++ Inheritance/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Car:Vehicle
+    {
+        public string type;
+        public static int numberOfCars;
+
+        public Car(string make, string model, int price): base (make, model, price)
+        {
+            this.type = "car";
+            numberOfCars++;
+        }
+    }
+}
