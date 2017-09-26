4d723bde2f0f272c0a88a14eecb649f4a081cc7d
raqeebuddin <raqeebuddin@hotmail.co.uk>
2017-09-26 15:40:29 +0100

    Class Setup

---
+++ Inheritance/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Vehicle
+    {
+        public string make;
+        public string model;
+        public int price;
+        public bool sold;
+
+        public Vehicle(string make, string model, int price)
+        {
+            this.make = make;
+            this.model = model;
+            this.sold = false;
+        }
+    }
+}
